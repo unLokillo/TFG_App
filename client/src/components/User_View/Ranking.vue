92b440751741d497b4a331166ae8cf474a658cd7
Rober <robertomontielrodriguez@gmail.com>
2022-05-10 17:51:26 +0200

    Print removed

--- client/src/components/User_View/Ranking.vue
+++ client/src/components/User_View/Ranking.vue
@@ -102,7 +102,6 @@ export default {
       .get("http://127.0.0.1:5000/login", { withCredentials: true })
       .then((response) => {
         this.logged = response.data;
-        console.log(this.logged);
       });
   },
   data() {
